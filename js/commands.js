7227f909c2635b72a03c4b0f2ab9641d498601ac
HyperSkys <66301237+HyperSkys@users.noreply.github.com>
2022-05-27 18:29:31 -0500

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,63 @@
+const youtube = "https://www.youtube.com/channel/UCUW7jV8cMWfAIG9WGovxE5g";
+const twitter = "https://www.twitter.com/HyperSkysMC";
+const onlyfans = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+const password = "sus";
+const github = "https://github.com/HyperSkys/";
+var email = 'mailto:contact@hyperskys.dev';
+
+whois = [
+    "<br>",
+    "Hey, I am HyperSkys! 👋",
+    "I'm a software engineer, who mainly does works on Java but also",
+    "does other things such as making websites, and other fun stuff I do when I'm bored.",
+    "I am currently a senior in high school and at the time of this making I have graduated,",
+    "I am planning on moving to California to take on my dream of being a software engineer.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'youtube        <a href="' + youtube + '" target="_blank">youtube/channel/UCUW7jV8cMWfAIG9WGovxE5g' + "</a>",
+    'twitter        <a href="' + twitter + '" target="_blank">twitter/hyperskysmc' + '</a>',
+    'github         <a href="' + github + '" target="_blank">github/hyperskys' + "</a>",
+    'onlyfans       <a href="' + onlyfans + '" target="_blank">onlyfans/hidden' + "</a>",
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    '<span class="command">obamaslastname</span>           Top Secret only for Administrators',
+    "<br>"
+];
+
+projects = [
+    "<br>",
+    "Uh no... most projects are private, on GitHub, or top secret.",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>          Who am I?',
+    '<span class="command">social</span>         Display Social Media',
+    '<span class="command">secret</span>         Find Secret Stuff',
+    '<span class="command">projects</span>       View Projects',
+    '<span class="command">history</span>        View Search History 😳',
+    '<span class="command">help</span>           I Have Fallen and Cannot Get Up',
+    '<span class="command">email</span>          Pointless to Email Me',
+    '<span class="command">clear</span>          Clear Search History',
+    '<span class="command">banner</span>         Display Start Menu',
+    "<br>",
+];
+
+banner = [
+    " __    __  ____    ____ .______    _______ .______              _______. __  ___ ____    ____  _______.",
+    "|  |  |  | \\   \\  /   / |   _  \\  |   ____||   _  \\            /       ||  |/  / \\   \\  /   / /       |",
+    "|  |__|  |  \\   \\/   /  |  |_)  | |  |__   |  |_)  |          |   (----`|  '  /   \\   \\/   / |   (----`",
+    "|   __   |   \\_    _/   |   ___/  |   __|  |      /            \\   \\    |    <     \\_    _/   \\   \\    ",
+    "|  |  |  |     |  |     |  |      |  |____ |  |\\  \\----.   .----)   |   |  .  \\      |  | .----)   |   ",
+    "|__|  |__|     |__|     | _|      |_______|| _| `._____|   |_______/    |__|\\__\\     |__| |_______/    ",
+    "                                                                                                       ",
+    '<span class="color2">Welcome to my awesome web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
