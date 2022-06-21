0190ab452e53155811a8dd7eb532f67adf604f18
Tresillocrack <tresillo2017@gmail.com>
2022-06-21 17:20:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ function enterKey(e) {
         if (e.keyCode === 13) {
             commands.push(command.innerHTML);
             git = commands.length;
-            addLine("guest@hyperskys.dev:~$ " + command.innerHTML, "no-animation", 0);
+            addLine("guest@tomasps.tk:~$ " + command.innerHTML, "no-animation", 0);
             commander(command.innerHTML.toLowerCase());
             command.innerHTML = "";
             textarea.value = "";
